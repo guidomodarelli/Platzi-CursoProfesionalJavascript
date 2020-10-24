53fede4e7e46d50243b7fc0201f44fecc204b2fa
guido <guidomodarelli25@gmail.com>
2020-10-24 15:35:16 -0300

    Tipos basicos typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -4,4 +4,30 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-const sum = add(2, 3)
+const sum = add(2, 3);
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push(900);
+peopleAndNumbers.push('Raul');
+
+// Enum
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
+}
+
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comdin: any = 'Joker';
+comdin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
