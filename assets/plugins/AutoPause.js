8485e9974acb411d6b8630d0515ba89bd4a3f362
guido <guidomodarelli25@gmail.com>
2020-10-24 13:00:29 -0300

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
@@ -10,6 +11,8 @@ class AutoPause {
       threshold: this.threshold,
     });
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
 
   handleIntersection(entries) {
@@ -17,6 +20,15 @@ class AutoPause {
 
     const isVisible = entry.intersectionRatio >= this.threshold;
 
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
     if (isVisible) {
       this.player.play();
     } else {
